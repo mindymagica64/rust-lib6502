8dd824d811974b680c8eb6aaadc37af1926fbe06
Mindy Davis <cubfan2509@gmail.com>
2015-09-01 08:46:14 -0700

    update to use env

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,8 +1,7 @@
 use lib6502::types;
 use lib6502::Register;
 use lib6502::Memory;
-use lib6502::AddressCalculator;
-use lib6502::Instructions;
+use lib6502::Core;
 
 mod lib6502 {
 
@@ -20,25 +19,25 @@ mod lib6502 {
 	impl CPU {
 		fn loadBytes(&mut self, data) {
 			std::slice::bytes::copy_memory(&data[..], &env.memory.rom[..]);
-			registers.program_counter = (memory.read(WithAddress(0xfffd)) as Address) << 8 | (memory.read(WithAddress(0xfffc)) as Address);
+			env.registers.program_counter = (env.memory.read(WithAddress(0xfffd)) as Address) << 8 | (env.memory.read(WithAddress(0xfffc)) as Address);
 		}
 
 		fn run_program(&mut self) {
 			if(!interactive){
 				while !finished {
-				 	Instructions::nextState(env);
+				 	Core::nextState(env);
 				 }
 			} else{
-				Instructions::nextState(env);
+				Core::nextState(env);
 			}
 		}
 
 		fn eval(&mut self, block) {
-			memory.rom = [0x0; 0x8000]
+			env.memory.rom = [0x0; 0x8000]
 			std::slice::bytes::copy_memory(&block[..], &memory.rom[..]);
-			registers.program_counter = 0x8000;
+			env.registers.program_counter = 0x8000;
 			while !finished {
-				Instructions::nextState(env);
+				Core::nextState(env);
 			}
 		}	
 	}
